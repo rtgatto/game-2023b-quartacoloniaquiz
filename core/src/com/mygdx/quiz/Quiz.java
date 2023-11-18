c5c48e272f77c4db6463f662fc521da67418163b
fclorenzo <comunellolorenzo@gmail.com>
2023-11-18 11:44:46 -0300

    Classe questão implementada

--- core/src/com/mygdx/quiz/Quiz.java
+++ core/src/com/mygdx/quiz/Quiz.java
@@ -1,4 +1,32 @@
 package com.mygdx.quiz;
 
+import java.util.List;
+
 public class Quiz {
+    private String pergunta;
+    private List<String> alternativas;
+    private int alternativaCorreta; // variável para guardar o índice da alternativa correta
+
+    public Quiz(String pergunta, List<String> alternativas, int alternativaCorreta) {
+        this.pergunta = pergunta;
+        this.alternativas = alternativas;
+        this.alternativaCorreta = alternativaCorreta;
+
+        if (alternativaCorreta < 0 || alternativaCorreta >= alternativas.size()) {
+            throw new IllegalArgumentException("Índice de alternativa correta está fora dos limites permitidos");
+        }
+    }
+
+    public String getPergunta() {
+        return pergunta;
+    }
+
+    public List<String> getAlternativas() {
+        return alternativas;
+    }
+
+    // verificar se a alternativa escolhida é a correta
+    public boolean alternativaECorreta(int alternativaEscolhida) {
+        return alternativaEscolhida == alternativaCorreta;
+    }
 }
