b64586f23c81169fddb45f9fb148fea66018c822
Leandro Dalla Nora <workdallanora@gmail.com>
2023-11-19 06:16:38 -0300

    Criação das telas do jogo

--- core/src/com/mygdx/quiz/QuartaColoniaQuiz.java
+++ core/src/com/mygdx/quiz/QuartaColoniaQuiz.java
@@ -1,31 +1,29 @@
 package com.mygdx.quiz;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.mygdx.quiz.screens.GameScreen;
 
-public class QuartaColoniaQuiz extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class QuartaColoniaQuiz extends Game {
+	private SpriteBatch batch;
+
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		this.setScreen(new GameScreen(this));
 	}
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void render() {
+		super.render();
 	}
-	
+
 	@Override
-	public void dispose () {
+	public void dispose() {
 		batch.dispose();
-		img.dispose();
 	}
 }
---
+++ core/src/com/mygdx/quiz/screens/GameScreen.java
@@ -0,0 +1,77 @@
+package com.mygdx.quiz.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.mygdx.quiz.QuartaColoniaQuiz;
+import sun.awt.X11.XSetWindowAttributes;
+
+public class GameScreen implements Screen {
+
+    final QuartaColoniaQuiz quiz;
+    private SpriteBatch batch;
+    private OrthographicCamera camera;
+    private Texture texture;
+    private ShapeRenderer shapeRenderer;
+
+
+    public GameScreen(QuartaColoniaQuiz quiz){
+        this.quiz = quiz;
+        batch = new SpriteBatch();
+        Gdx.graphics.setWindowedMode(1370, 715);
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 3400, 2323); 
+        texture = new Texture(Gdx.files.internal("mapa.png"));
+        shapeRenderer = new ShapeRenderer();
+    }
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0, 0, 0.2f, 1);
+
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+        shapeRenderer.setProjectionMatrix(camera.combined);
+
+        batch.begin();
+        batch.draw(texture, (1370 - texture.getWidth()) / 2f, (715 - texture.getHeight()) / 2f);
+        batch.end();
+
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        texture.dispose();
+        shapeRenderer.dispose();
+    }
+}
