89bb231a242a15ee2dc69a5426785831304f945a
Leandro Dalla Nora <workdallanora@gmail.com>
2023-11-21 10:46:10 -0300

    Desenvolvimento da tela prinicpal do jogo. Não funcional

--- core/src/com/mygdx/quiz/screens/GameScreen.java
+++ core/src/com/mygdx/quiz/screens/GameScreen.java
@@ -4,11 +4,13 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.mygdx.quiz.QuartaColoniaQuiz;
-import sun.awt.X11.XSetWindowAttributes;
 
 public class GameScreen implements Screen {
 
@@ -17,16 +19,23 @@ public class GameScreen implements Screen {
     private OrthographicCamera camera;
     private Texture texture;
     private ShapeRenderer shapeRenderer;
-
+    private Sprite sprite;
+    Rectangle bounds;
+    TextureRegion region;
 
     public GameScreen(QuartaColoniaQuiz quiz){
         this.quiz = quiz;
         batch = new SpriteBatch();
         Gdx.graphics.setWindowedMode(1370, 715);
         camera = new OrthographicCamera();
-        camera.setToOrtho(false, 3400, 2323); 
+        camera.setToOrtho(false, 800, 549);
         texture = new Texture(Gdx.files.internal("mapa.png"));
         shapeRenderer = new ShapeRenderer();
+        sprite = new Sprite(texture);
+
+        TextureRegion region = new TextureRegion(Gdx.files.internal("mapa.png"));
+        Rectangle bounds = new Rectangle(0, 0, region.getRegionWidth(), region.getRegionHeight());
+
     }
     @Override
     public void show() {
@@ -42,14 +51,15 @@ public class GameScreen implements Screen {
         shapeRenderer.setProjectionMatrix(camera.combined);
 
         batch.begin();
-        batch.draw(texture, (1370 - texture.getWidth()) / 2f, (715 - texture.getHeight()) / 2f);
+        sprite.draw(batch);
         batch.end();
-
-
     }
 
     @Override
     public void resize(int width, int height) {
+
+
+//        sprite.setPosition(50, 200);
 
     }
 
