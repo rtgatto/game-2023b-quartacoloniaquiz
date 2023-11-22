1315ba61cc155b414fa18558c7f8aa9d7bf1c209
Leandro Dalla Nora <workdallanora@gmail.com>
2023-11-22 19:10:37 -0300

    Alterações na tela do jogo

--- core/src/com/mygdx/quiz/screens/GameScreen.java
+++ core/src/com/mygdx/quiz/screens/GameScreen.java
@@ -6,36 +6,31 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.mygdx.quiz.Event;
 import com.mygdx.quiz.QuartaColoniaQuiz;
 
 public class GameScreen implements Screen {
 
-    final QuartaColoniaQuiz quiz;
+    final QuartaColoniaQuiz game;
+    private Event event;
     private SpriteBatch batch;
     private OrthographicCamera camera;
     private Texture texture;
     private ShapeRenderer shapeRenderer;
     private Sprite sprite;
-    Rectangle bounds;
-    TextureRegion region;
 
-    public GameScreen(QuartaColoniaQuiz quiz){
-        this.quiz = quiz;
+
+    public GameScreen(QuartaColoniaQuiz game){
+        this.game = game;
         batch = new SpriteBatch();
-        Gdx.graphics.setWindowedMode(1370, 715);
+        Gdx.graphics.setWindowedMode(1250, 250); // W: 1370
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 549);
         texture = new Texture(Gdx.files.internal("mapa.png"));
         shapeRenderer = new ShapeRenderer();
         sprite = new Sprite(texture);
-
-        TextureRegion region = new TextureRegion(Gdx.files.internal("mapa.png"));
-        Rectangle bounds = new Rectangle(0, 0, region.getRegionWidth(), region.getRegionHeight());
-
     }
     @Override
     public void show() {
@@ -57,10 +52,6 @@ public class GameScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
-
-//        sprite.setPosition(50, 200);
-
     }
 
     @Override
